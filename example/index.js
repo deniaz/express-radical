7bd6a3144bc3b3b86b74dd6e0996cb14d3f2eb03
Robert Vogt <robert@smartive.ch>
2017-02-08 08:08:22 +0100

    Move example code and introduce lib-structure

--- example/index.js
+++ example/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 
-const radical = require('./lib')
+const radical = require('../src')
 // radical(app, [
 //   require('./src/product'),
 //   require('./src/person'),
 // ])
 
-radical(app, './src')
+radical(app, './schemas')
 
 app.get('/', (req, res) => res.sendStatus(418))
 app.listen(8080, () => console.info(`Listening on *:8080`))
